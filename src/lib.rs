e7e32f7351758c6c5ed581dd3f61076301ba2dd6
Christian Howe <cj@cjhowe.dev>
2023-02-25 01:26:39 -0800

    Fix unit test

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,8 @@ mod tests {
     fn freetype_init() {
         let mut library = std::ptr::null_mut();
         unsafe {
-            FT_Init_FreeType(&mut library);
-            FT_Done_FreeType(library);
+            assert_eq!(FT_Init_FreeType(&mut library), FT_Err_Ok);
+            assert_eq!(FT_Done_FreeType(library), FT_Err_Ok);
         }
     }
 }
